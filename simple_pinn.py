2ff832aa1bfb4e45287433bb8a6e448e77eab3ed
kinchb <brooks.e.kinch@gmail.com>
2024-01-23 17:15:49 -0800

    this barely works, but is qualitatively the correct evolution of the brio and wu problem

--- simple_pinn.py
+++ simple_pinn.py
@@ -33,3 +33,53 @@ class SimplePINN(nn.Module):
             x = self.activation(x)
         x = self.head(x)
         return x
+
+
+# this version forces B_x equal to some supplied constant, as in the Brio and Wu shock tube problem,
+# obviating the need for the monopole loss
+class BrioAndWuPINN(SimplePINN):
+    def __init__(
+        self,
+        input_size,
+        hidden_layers,
+        output_size,
+        B_x=0.75,
+        activation=nn.ReLU(),
+        use_bias_in_output_layer=False,
+    ):
+        super(BrioAndWuPINN, self).__init__(
+            input_size,
+            hidden_layers,
+            output_size - 1,
+            activation,
+            use_bias_in_output_layer,
+        )
+        self.B_x = B_x
+
+    def forward(self, x):
+        for layer in self.hidden_layers:
+            x = layer(x)
+            x = self.activation(x)
+        x = self.head(x)
+        B_x = self.B_x * torch.ones(x.shape[:-1]).unsqueeze(-1).to(x.device)
+        x = torch.cat([x[..., :4], B_x, x[..., 4:]], dim=-1)
+        return x
+
+
+if __name__ == "__main__":
+    mhd_state_variables_nn = BrioAndWuPINN(
+        2,
+        [32, 32, 32, 32, 32],
+        8,
+        activation=nn.Softplus(),
+    )
+
+    Nt = 101
+    Nx = 301
+
+    t = torch.linspace(0.0, 0.2, Nt, requires_grad=True)
+    x = torch.linspace(-1.0, 1.0, Nx, requires_grad=True)
+    T, X = torch.meshgrid(t, x, indexing="ij")
+    inputs = torch.stack([T, X], dim=len(T.shape))
+
+    mhd_state_variables = mhd_state_variables_nn(inputs)
